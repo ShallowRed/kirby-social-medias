2a93cd69b947e8078bcef429456c3a5b5b782de3
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2024-10-14 11:23:44 +0200

    Wrap links in container

--- snippets/blocks/social-media-list.php
+++ snippets/blocks/social-media-list.php
@@ -1,5 +1,7 @@
+<div class="social-media-list">
 <?php
-
 foreach ($block->content()->socialmedialist()->toStructure() as $socialMediaItem) :
     snippet('social-media-link', ['socialMediaItem' => $socialMediaItem]);
 endforeach;
+?>
+</div>
